3cb31d6326284924e32b3a56cae3daef39d1f610
dimit <dimitar_rradev@abv.bg>
2024-12-18 10:43:02 +0200

    first commit

---
+++ src/main/java/bg/softuni/springDataXmlProcessingPartTwo/SpringDataXmlProcessingPartTwoApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.springdataxmlprocessingparttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataXmlProcessingPartTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataXmlProcessingPartTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/springDataXmlProcessingPartTwo/SpringDataXmlProcessingPartTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.springdataxmlprocessingparttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataXmlProcessingPartTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
